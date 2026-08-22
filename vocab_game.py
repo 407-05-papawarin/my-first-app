75e1591c737c8fc5607b8c177e5fb315f4f1dbda
407-05-papawarin <54144@yupparaj.ac.th>
2026-08-22 18:02:46 +0700

    Update vocab_game.py

--- vocab_game.py
+++ vocab_game.py
@@ -53,14 +53,14 @@ def show_result_dialog(ans1, ans2):
 
     # ตรวจข้อ 3 
     if u_ans3 == "banana":
-        st.session("✅ ข้อ 3: ถูกต้อง")
+        st.success("✅ ข้อ 3: ถูกต้อง")
         score += 1
     else:
         st.error(f"❌ ข้อ 3 ยังไม่ถูกต้อง (คุณตอบ '{u_ans3}')")
 
     # ตรวจข้อ 4 
     if u_ans4 == "blueberry":
-        st.session("✅ ข้อ 4: ถูกต้อง")
+        st.success("✅ ข้อ 4: ถูกต้อง")
          score += 1
     else:
         st.error(f"❌ ข้อ 4: ยังไม่ถูกต้อง (คุณตอบ '{u_ans4}')")
@@ -127,7 +127,7 @@ if "start" in st.session_state and not st.session_state.get("is_ended", False):
 
 # 5. แสดง Dialog ผลลัพธ์
 if st.session_state.get("is_ended", False):
-    show_result_dialog(ans1, ans2, ans3, ans4)
+    show_result_dialog(ans1, ans2, ans3, ans4):
 
 st.divider()
 st.write("นางสาวปภาวรินท์ มณีเกี่๋ยง เลขที่ 5 ม.4/7")
