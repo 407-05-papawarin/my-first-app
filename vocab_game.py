dad9ff7b2afc9e97e9254c3bb62749b21379eae5
407-05-papawarin <54144@yupparaj.ac.th>
2026-08-22 18:04:43 +0700

    Update vocab_game.py

--- vocab_game.py
+++ vocab_game.py
@@ -61,7 +61,7 @@ def show_result_dialog(ans1, ans2):
     # ตรวจข้อ 4 
     if u_ans4 == "blueberry":
         st.success("✅ ข้อ 4: ถูกต้อง")
-         score += 1
+        score += 1
     else:
         st.error(f"❌ ข้อ 4: ยังไม่ถูกต้อง (คุณตอบ '{u_ans4}')")
 
